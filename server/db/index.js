20c96fd223c51140d019f4335645e6179f0f1f39
m33dusa <zainab16@bu.edu>
2021-02-23 13:04:30 -0600

    removing db info in db index.js

--- server/db/index.js
+++ server/db/index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connection_string = process.env.MONGO_CONNECTION_STRING || 'mongodb+srv://teamBat:0dptsWPsbSufEtcR@cscl.7cnhq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority/cscl';
 
 mongoose
-    .connect(connection_string, {dbName: 'cscl', user:'teamBat', pass:'0dptsWPsbSufEtcR', useNewUrlParser: true, useUnifiedTopology:true})
+    .connect(connection_string, {useNewUrlParser: true, useUnifiedTopology:true})
     .catch(e =>{
         console.error('Connection error', e.message);
     });
